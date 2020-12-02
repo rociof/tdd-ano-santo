dec5eb35779adee6476dca0a90096262e3521676
Aula <Aula@EQUIP079576.xunta.local>
2020-12-02 11:05:12 +0100

    test

--- index.js
+++ index.js
@@ -5,8 +5,12 @@
  * @returns true o false, en función de si es año santo o no.
  */
 function anoSanto(n) {
+  //La periodicidad (partiendo del 2021) es 6-5-6-11 para este siglo
   //TODO: realizar cálculos oportunos
-  if (n==2021) return true;
+  var dia = "25";
+  var mes = "7";
+  var fecha = dia + "/" + mes + "/" + n;
+  if (fecha = 7) return true;
   return false;
 }
 
--- test.js
+++ test.js
@@ -2,6 +2,10 @@ import { anoSanto } from './index';
 import { strictEqual } from "assert";
 
 describe(('Comprobación de año santo'), () => {
-  it('test 2020', () => { strictEqual(anoSanto(2020), false) })
-  it('test 2021', () => { strictEqual(anoSanto(2021), true) })
+  it('test 2020 no es', () => { strictEqual(anoSanto(2020), false) })
+  it('2021 es santo', () => { strictEqual(anoSanto(2021), true) })
+  it('2027 es santo', () => { strictEqual(anoSanto(2027), true) })
+  it('2032 es santo', () => { strictEqual(anoSanto(2027), true) })
+  it('2033 no es santo', () => { strictEqual(anoSanto(2027), true) })
+
 })
