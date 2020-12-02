e888e2034b1dd4e7b1c20b87174e3dcf57dc880b
Aula <Aula@EQUIP079576.xunta.local>
2020-12-02 13:50:34 +0100

    cambios2

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+/*La periodicidad (partiendo del 2021) es 6-5-6-11 para este siglo 
+    a partir del año "100" (el último de cada siglo) se rompe el ciclo.
+    Año bisiesto es cada 4 años salvo que sea divisibles por cien, pero no por cuatrocientos*/
 
 /**
  * Función para determinar si un año n es considerado Año Santo.
@@ -5,7 +8,7 @@
  * @returns true o false, en función de si es año santo o no.
  */
 // function anoSanto(n) {
-//   //La periodicidad (partiendo del 2021) es 6-5-6-11 para este siglo
+//   
 //   //TODO: realizar cálculos oportunos
 //   var dia = "25";
 //   var mes = "7";
@@ -15,16 +18,21 @@
 // }
 
 function anoSanto(n) {
-  //TODO: realizar cálculos oportunos
+
+  //funcionaría para los siglos XX, XXI y XXII (de 1900 hasta 2200)
+  if (n> 2100) n--;
+  while (n<2021) {
+    n += 28;
+  }
+  while (n>2038) {
+    n -= 28;
+  }
   if (n==2021) return true;
   if (n==2027) return true;
   if (n==2032) return true;
   if (n==2038) return true;
-  if (n==2049) return true;
   return false;
 }
-
-
 export {
   anoSanto
 }
--- test.js
+++ test.js
@@ -9,8 +9,13 @@ describe(('Comprobación de año santo'), () => {
   it('2033 no es santo', () => { strictEqual(anoSanto(2033), false) })
   it('2038  es santo', () => { strictEqual(anoSanto(2038), true) })
   it('2040 no es santo', () => { strictEqual(anoSanto(2040), false) })
-  it('2049  es santo', () => { strictEqual(anoSanto(2027), true) })
-
-
-
+  it('2049  es santo', () => { strictEqual(anoSanto(2049), true) })
+  it('2054 no es santo', () => { strictEqual(anoSanto(2054), false) })
+  it('2055  es santo', () => { strictEqual(anoSanto(2055), true) })
+  it('2094   es santo', () => { strictEqual(anoSanto(2094), true) })
+  it('2105 no es santo', () => { strictEqual(anoSanto(2105), false) })
+  it('2106   es santo', () => { strictEqual(anoSanto(2106), true) })
+  it('2107  no es santo', () => { strictEqual(anoSanto(2107), false) })
+  it('1993   es santo', () => { strictEqual(anoSanto(1993), true) })
+  it('1994  no es santo', () => { strictEqual(anoSanto(1994), false) })
 })
